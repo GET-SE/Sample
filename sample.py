93b550e6e55bd19c72ab03b0dfe7c3f36d6a0b8d
gayathri <135581978+gayathriswarnamukhi@users.noreply.github.com>
2023-06-28 13:45:44 +0530

    Added new print Command

--- sample.py
+++ sample.py
@@ -2,6 +2,10 @@ print("helo")
 print("Hi SE TEAM")
 
 # This is the comment
+<<<<<<< Updated upstream
 print("it is sample")
 print("active -- class")
 print("Project active class")
+=======
+print("Hello Team" )
+>>>>>>> Stashed changes
